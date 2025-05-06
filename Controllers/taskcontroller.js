7abd0754465ebf49a676e50ca2a8d43957a6ab16
TylerEscuriex <111813758+TylerEscuriex@users.noreply.github.com>
2025-05-06 18:25:38 -0500

    Update taskcontroller.js

--- Controllers/taskcontroller.js
+++ Controllers/taskcontroller.js
@@ -1,4 +1,7 @@
 const Task = require('../models/Task');
+const { MongoClient } = require("mongodb");
+// The uri string must be the connection string for the database (obtained on Atlas).
+const uri = "mongodb+srv://tylerescuriex:TBa1CJQFexW4Q1mi@temdb.n06hy6j.mongodb.net/?retryWrites=true&w=majority&appName=temdb";
 
 // Create a new task
 exports.createTask = async (req, res) => {
